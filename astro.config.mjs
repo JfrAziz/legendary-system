f9e4e52a07dd4ac657c0437c02c5f17e8edfdfdd
Jafar Aziz <aziz@jafaraziz.com>
2023-01-23 08:45:01 +0700

    setup prettier for code formatting

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,25 +3,25 @@ import react from "@astrojs/react";
 import image from "@astrojs/image";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   markdown: {
     shikiConfig: {
-      theme: 'github-light'
-    }
+      theme: "github-light",
+    },
   },
   integrations: [
     react(),
     mdx({
-      syntaxHighlight: 'shiki',
-      shikiConfig: { theme: 'github-light' }
+      syntaxHighlight: "shiki",
+      shikiConfig: { theme: "github-light" },
     }),
     sitemap(),
     image({
-      serviceEntryPoint: '@astrojs/image/sharp'
+      serviceEntryPoint: "@astrojs/image/sharp",
     }),
-    tailwind()
-  ]
+    tailwind(),
+  ],
 });
